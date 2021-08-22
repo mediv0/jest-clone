b6a4b268591457cd6939d16d41a000a7de3d37e6
mediv0 <anonymousaxis@gmail.com>
2021-08-22 17:08:21 +0430

    update tests

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,6 +1,8 @@
+const sum = (a, b) => {
+    return a + b;
+}
+
 group("group-1", () => {
-    test("1 should be equal to 1", 1).toBe(2);
-    test("3 should be equal to 3", 3).toBe(2);
-    test("3 should be equal to 3", 3).toBe(3);
-    test("3 should be equal to 3", 3).toBe(3);
+    const r1 = sum(1, 2);
+    test("1 + 2 should be 3", r1).toBe(3);
 });
