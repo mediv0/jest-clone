017e6310db43647f5fb2ffb4009a635752dde07c
mediv0 <anonymousaxis@gmail.com>
2021-08-22 17:27:54 +0430

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,7 +2,20 @@ const sum = (a, b) => {
     return a + b;
 }
 
-group("group-1", () => {
+const reverse = (arr) => {
+    return arr.reverse();
+}
+
+const returnDummyString = () => "dummy";
+
+group("test group", () => {
     const r1 = sum(1, 2);
     test("1 + 2 should be 3", r1).toBe(3);
+    
+    const r2 = returnDummyString();
+    test("result should be string", typeof r2).toBe("string");
+
+    const r3 = reverse([1,2,3]);
+    test("should reverse array", r3).toBe([4,2,1]);
+
 });
